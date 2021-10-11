352df721fc79c8358fa1f23980c8dd8a502c8f92
odelyaRachamim <odelush22@gmail.com>
2021-10-11 22:57:47 +0300

    ex 2.5 Toggle Button- DoNe!

--- src/components/ToggleButton/ToggleButton.js
+++ src/components/ToggleButton/ToggleButton.js
@@ -13,23 +13,8 @@ function ToggleButton() {
 export default ToggleButton;
 
 {
-  /* import {useState} from "react";
-
-function ToggleButton ()
-{
-  const [myString, setString] = useState(true);
-  return (
-    <div>
-     <button onClick={() => setString(!myString)}> Click here for hidding the string<button/>
-     <h2>{myString ? "HELLO" :""}<h2/>
-    <div/>
-  );
-}
-
-export deafult ToggleButton;
-===============================================================
-
-export default function ToggleButton() {
+  /* 
+  export default function ToggleButton() {
   return <div> my Toggle button! </div>;
 } */
 }
